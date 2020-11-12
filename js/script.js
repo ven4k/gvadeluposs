f1d6b257651d156167e2debf2789d091046b6867
ven4k <barryven@yandex.ru>
2020-11-12 19:04:21 +0300

    button

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
 let todoList = [];
 
-function renderTask() {
-	todoList.forEach(function(task) {
-	todoList.reverse().splice(0, todoList.length);
-})};
-
+	function renderTask() {
+	todoList.forEach(function(item){
+		let taskInfo = item.text;
+		$('.list-item').append($('<div>', {
+	'class': 'newItem',		
+    'text': taskInfo,
+		}));
+		$('.newItem').append($('<button>', {
+			'class': 'removeBtn',
+			'text': 'Delete',
+		}))
+		})}
 
 	$('.btn').click(function() {
 	let value = $('input').val();
@@ -13,13 +20,17 @@ function renderTask() {
 		text: value,
 		completed: false,
 	}
-	if(task.value !== ''){
-	todoList.push(task);
+	if(value === ''){
+	return console.log('task value = none');
+	}else{
+		todoList.push(task);
 	}
-	console.log(task);
+	console.log(todoList);
 	$('input').val('');
+	$('.list-item').empty();
 	renderTask(todoList);
-	$('.list-item').append(task.text);
-    $('.list-item').wrapInner(`<div class="newItem"></div>`);
+	
 });
-
+	$('.removeBtn').click(function() {
+		$('.newItem').closest().remove();
+	});
