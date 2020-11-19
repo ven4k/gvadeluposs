f1f19aac35ccf4a7dea103bce910f255d993427d
ven4k <barryven@yandex.ru>
2020-11-19 19:22:37 +0300

    checkbox

--- js/script.js
+++ js/script.js
@@ -1,16 +1,30 @@
 let todoList = [];
 
-	function renderTask() {
-	todoList.forEach(function(item){
-		let taskInfo = item.text;
-		$('.list-item').append($('<div>', {
-	'class': 'newItem',		
-    'text': taskInfo,
-		}));
-		
-		})}
+	function getTemplate(text, id) {
+		return `
+			<div class="todo">
+				<span class="todo__text">
+				<input type="checkbox" class="todo__checkbox" value="${id}" name="option1">
+				${text}
+				</span>
+				<button type="button" class="todo__button" value="${id}">
+					Delete
+				</button>
 
-	$('.btn').click(function() {
+			</div>
+		`
+	}
+
+	function renderTask(tasks) {
+		$('.list').empty();
+	todoList.forEach(function(item){
+		let template = getTemplate(item.text, item.id);
+		$('.list').append(template)
+	})
+}
+
+	$('.btn').click(function(e) {
+	e.preventDefault();
 	let value = $('input').val();
 	let task = {
 		id: Date.now(),
@@ -24,16 +38,60 @@ let todoList = [];
 	}
 	console.log(todoList);
 	$('input').val('');
-	$('.list-item').empty();
-	renderTask(todoList);
-	$('.newItem').append($('<button>', {
-			'class': 'removeBtn',
-			'text': 'Delete',
-		}))
-		
+	$('.list').empty();
+	renderTask(todoList)
 });
-	$('.list-item').on('click','button.removeBtn', function(){
-  let del = $(this).closest('div');
-  todoList.splice(del.index(), 1);
-  del.remove();
-});
+	$('.list').on('click', '.todo__button', function(e) {
+		e.preventDefault();
+		const id = e.target.value;
+		const index = todoList.indexOf((item) => item.id === Number(id));
+		todoList.splice(index, 1);
+		renderTask(todoList);
+	});
+
+	$('.remover').on('click', '.todo__buttonDeleteAll', function(e) {
+		todoList.splice(0);
+		renderTask(todoList);
+	});
+
+
+
+	$('.list').on('click', '.todo__checkbox', function (e) {
+		const target = e.target;
+		const id = e.target.value;
+		const index = todoList.indexOf((item) => item.id === Number(id));
+		if ($(target).is(':checked')) {
+		todoList.map((index, item) => item.completed = true);
+		$(target).parent().css({
+			'color': 'red',
+			'font-weight': 'bold'
+	})
+		console.log(true);
+	} else {
+		todoList.map(index, (item) => item.completed = false);
+		$(target).parent().css({
+			'color': '#000',
+			'font-weight': 'normal'
+	})
+		console.log(false);
+	}
+	})
+	
+	$('.remover').on('click', '.todo__buttonSelect', function(e) {	
+		 
+		 $('.todo__checkbox').each(function() { 
+		this.checked = true;
+		todoList.map((item) => item.completed = true);
+		$('.list').parent().css({
+			'color': 'red',
+			'font-weight': 'bold'
+	})
+	})})
+	$('.remover').on('click', '.todo__buttonUnSelect', function(e) {	
+		 $('.todo__checkbox').each(function() { 
+			this.checked = false;
+		 $('.list').parent().css({
+			'color': '#000',
+			'font-weight': 'normal'
+	})
+	})})
